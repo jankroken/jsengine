6a134cce31c8a06eaa46c94d77c52b688e09209d
jankroken <jan.kroken@gmail.com>
2011-09-09 21:12:20 +0200

    simple argument now working, but scope not implemented

--- src/main/scala/jsengine/runtime/tree/RTTry.scala
+++ src/main/scala/jsengine/runtime/tree/RTTry.scala
@@ -3,6 +3,8 @@ package jsengine.runtime.tree
 import jsengine.runtime.ExecutionContext
 
 class RTTry() extends RTExpression {
-		def evaluate(env: RTEnvironmentRecord):RTObject = { throw new RuntimeException("Not implemented") }
+  def evaluate(env: RTEnvironmentRecord): RTObject = {
+    throw new RuntimeException("Not implemented")
+  }
 
 }
--- src/main/scala/jsengine/runtime/tree/RTUserFunction.scala
+++ src/main/scala/jsengine/runtime/tree/RTUserFunction.scala
@@ -3,34 +3,45 @@ package jsengine.runtime.tree
 import jsengine.runtime.ExecutionContext
 import jsengine.runtime.library._
 
-class RTUserFunction(val name: Option[RTId],val args: List[RTId],val decl: List[RTId],val source: List[RTExpression]) extends RTFunction {
-	var environment: Option[RTEnvironmentRecord] = None
-  
-  	override def evaluate(env: RTEnvironmentRecord):RTObject = { environment = Some(env); this}
-  	override def call(callObject: CallObject):RTObject = {
-		var retValue:RTObject = Stdlib_Undefined
-		for (expr <- source) {
-		  environment match {
-		    case None => throw new RuntimeException("environment is not set")
-		    case Some(env) => {
-		    	retValue = expr.evaluate(env).valueOf
-		    }
-		  }
-		}
-		retValue
-  	}
-  	override def toBoolean():Stdlib_Boolean = { Stdlib_Boolean(true) }
-  	override def toString():String = {
-  		"function("+name+","+args+","+decl+","+source+")"
-  	}
-  	
-  	override def isObject = true
-	override def isPrimitive = false
+class RTUserFunction(val name: Option[RTId], val args: List[RTId], val decl: List[RTId], val source: List[RTExpression]) extends RTFunction {
+  var environment: Option[RTEnvironmentRecord] = None
+
+  override def evaluate(env: RTEnvironmentRecord): RTObject = {
+    environment = Some(env); this
+  }
+
+  override def call(callObject: CallObject): RTObject = {
+    val environment = new RTEnvironmentRecord
+    for ((id,value) <- args.zip(callObject.args)) {
+        println(id + " <- "+value)
+      environment.declare(id)
+      environment.getReference(id) match {
+        case ref: RTReference => ref.setValue(value)
+      }
+    }
+    var retValue: RTObject = Stdlib_Undefined
+    for (expr <- source) {
+      retValue = expr.evaluate(environment).valueOf
+    }
+    retValue
+  }
+
+  override def toBoolean(): Stdlib_Boolean = {
+    Stdlib_Boolean(true)
+  }
+
+  override def toString(): String = {
+    "function(" + name + "," + args + "," + decl + "," + source + ")"
+  }
+
+  override def isObject = true
+
+  override def isPrimitive = false
 
 }
 
 object RTUserFunction {
-	def apply(name: Option[RTId],args: List[RTId],decl: List[RTId],source: List[RTExpression]):RTUserFunction = {
-		new RTUserFunction(name,args,decl,source)
-	}  
+  def apply(name: Option[RTId], args: List[RTId], decl: List[RTId], source: List[RTExpression]): RTUserFunction = {
+    new RTUserFunction(name, args, decl, source)
+  }
 }
