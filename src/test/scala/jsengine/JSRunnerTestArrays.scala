5d803ac915f91c5716023dab5ada432cf17015b1
jankroken <jan.kroken@gmail.com>
2011-09-20 21:45:00 +0200

    Added a couple of more unit tests for arrays, and by pure luck, they passed without requiring any changes :)

--- src/test/scala/jsengine/JSRunnerTestArrays.scala
+++ src/test/scala/jsengine/JSRunnerTestArrays.scala
@@ -7,12 +7,25 @@ import org.hamcrest.CoreMatchers.is
 
 class JSRunnerTestArrays {
 
-	@Test def testBlockNotObject() {
+	@Test def testSimpleArray() {
 		val source = "var a = [4,8,15,16,23,42]; a[0]"
     val expected = ScalaReturnDouble(4.0)
 		val retval = new JSRunner().run(source)
 		assertThat(retval,is[Any](expected))
 	}
 
+  @Test def testInsertingIntoEmptyArray() {
+    val source = "var a = []; a[0] = 42; a[0]"
+    val expected = ScalaReturnDouble(42.0)
+    val retval = new JSRunner().run(source)
+    assertThat(retval,is[Any](expected))
+  }
+
+  @Test def testInsertingWithInterval() {
+    val source = "var a = [0]; a[4] = 4; a[2]"
+    val expected = ScalaReturnUndefined
+    val retval = new JSRunner().run(source)
+    assertThat(retval,is[Any](expected))
+  }
 }
 
